c6065f407439ce76e7571c8ffcf05c68b26f8462
Lucas Bonilla <lucashernandobonilla@gmail.com>
2023-01-30 13:09:30 -0500

    Proyecto React Lucas

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/headerComponent'
+import FormSearchCity from './components/SearchCityComponent';
+
+
+function App() {
+  return (
+    
+      <><Header /><FormSearchCity/></>
+    
+  );
+}
+
+export default App;
